3cdd3893c92fef1dcd299dbb8e1186dc448c5927
Xgor <real.xgor@hotmail.com>
2017-07-14 21:37:17 +0200

    First Commit

---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public float acceleration;
+	public float friction;
+	public float groundTurnSpeed;
+	public float airTurnSpeed;
+	public float jetpackPower;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
