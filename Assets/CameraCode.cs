904a9f7a4a1d4f10d4423c7d6b18f88f4d497e3a
Xgor <real.xgor@hotmail.com>
2017-07-14 22:17:17 +0200

    Moving Carrr

--- Assets/CameraCode.cs
+++ Assets/CameraCode.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class CameraCode : MonoBehaviour {
 
+	public Transform target;
+	public Vector3 offset;
+
 	// Use this for initialization
 	void Start () {
-		
+		if(offset == Vector3.zero)
+		{
+			offset = transform.position;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.position = offset+target.position;
 	}
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,22 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof(Rigidbody))]
 public class Player : MonoBehaviour {
+
 	public float acceleration;
 	public float friction;
 	public float groundTurnSpeed;
 	public float airTurnSpeed;
 	public float jetpackPower;
+	public float jetpackFuel;
+	float currentFuel;
+	public GameObject groundFire;
+	public GameObject airFire;
 
-
+	Vector3 v;
+	Rigidbody rig;
+	bool onGround;
 
 	// Use this for initialization
 	void Start () {
-		
+		rig = GetComponent<Rigidbody>();
+
+	}
+
+	void Update()
+	{
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+
+
+
+		onGround =transform.position.y < 0;
+			
+
+		if(onGround)
+		{
+
+			groundFire.SetActive(true);
+			Movement();
+			currentFuel = jetpackFuel;
+			transform.Rotate(Vector2.up*groundTurnSpeed*Input.GetAxis("Horizontal"));
+		}
+		else
+		{
+			groundFire.SetActive(false);
+			transform.Rotate(Vector2.up*airTurnSpeed*Input.GetAxis("Horizontal"));
+		}
+
+		Jetpack();
+	}
+
+	void Movement()
+	{
+		v = rig.velocity;
+		v -= friction * v *Time.deltaTime;
+		v += acceleration * transform.forward * Time.deltaTime;
+		v.y = rig.velocity.y;
+		rig.velocity = v;
+	}
+
+	void Jetpack()
+	{
+		if(Input.GetButton("Fire1") && 0 < currentFuel)
+		{
+			airFire.SetActive(true);
+			currentFuel -= Time.deltaTime;
+			rig.AddForce(Vector3.up*jetpackPower);
+		}
+		else
+		{
+			airFire.SetActive(false);
+		}
 	}
 }
